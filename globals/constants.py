1f3c2ece006e8ae60a6be85924cb1c072265f002
JHP-3101 <joseph.h.prabawa@mu.co.id>
2025-04-17 14:30:50 +0700

    Member Menu Development

--- globals/constants.py
+++ globals/constants.py
@@ -1,6 +1,12 @@
-# Menus
+# Main Menu
 MEMBER = "member-menu"
 ON_DEV_1 = "on-dev-menu1"
+
+# Member Menu
+MEMBER_MENU_MAIN = "member-menu"
+MEMBER_MENU_INFO = "member-info"
+MEMBER_MENU_REGISTER = "member-register"
+MEMBER_MENU_CHECK_POINTS = "member-check-points"
 
 # Messages Handler
 TEXT_MESSAGE = "text"
--- handlers/message_handler.py
+++ handlers/message_handler.py
@@ -28,15 +28,33 @@ class MessageHandler:
             if list_reply_id == constants.MEMBER:
                 response_text = "Anda memilih menu Member."
                 logger.info(f"Handling list reply for Member: {list_reply_id} from {from_no}")
-                await self.whatsapp_service.send_message(from_no, response_text)
-                
-                # Future: Trigger member-related logic here
+                await self.whatsapp_service.send_member_menu(from_no)
+            
+            # Future: Trigger member-related logic here 
             elif list_reply_id == constants.ON_DEV_1:
                 response_text = "Menu ini sedang dalam pengembangan."
                 logger.info(f"Handling list reply for On Development: {list_reply_id} from {from_no}")
                 await self.whatsapp_service.send_message(from_no, response_text)
-                
                 # Future: Indicate that this is under development
+                
+            elif list_reply_id == constants.MEMBER_MENU_INFO:
+                response_text = "Anda memilih menu Informasi Member."
+                logger.info(f"Handling list reply for Informasi Member: {list_reply_id} from {from_no}")
+                await self.whatsapp_service.send_message(from_no, response_text)
+                # Logical Funtion To Show Member Info
+                
+            elif list_reply_id == constants.MEMBER_MENU_REGISTER:
+                response_text = "Anda memilih menu Daftar Member."
+                logger.info(f"Handling list reply for Daftar Member: {list_reply_id} from {from_no}")
+                await self.whatsapp_service.send_message(from_no, response_text)
+                # Logical Funtion to Register Member
+                
+            elif list_reply_id == constants.MEMBER_MENU_CHECK_POINTS:
+                response_text = "Anda memilih menu Cek Poin Member."
+                logger.info(f"Handling list reply for Cek Poin Member: {list_reply_id} from {from_no}")
+                await self.whatsapp_service.send_message(from_no, response_text)
+                # Logical Funtion To Check Member Points
+            
             else:
                 response_text = f"Anda memilih: {list_reply_id}"
                 logger.warning(f"Unhandled list reply ID: {list_reply_id} from {from_no}")

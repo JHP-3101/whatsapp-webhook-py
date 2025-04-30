c19a4b8793756b9909b919f51a597697a09b2ae9
Joseph Heykel Prabawa <josephheykel@gmail.com>
2025-04-30 14:50:15 +0700

    development/session manager and handler adding lifespan

--- controllers/lifespan.py
+++ controllers/lifespan.py
@@ -3,6 +3,9 @@ from contextlib import asynccontextmanager
 from services.session_manager import SessionManager
 from services.whatsapp_service import WhatsAppService
 from handlers.message_handler import MessageHandler
+from core.logger import get_logger
+
+logger = get_logger()
 
 whatsapp_service = WhatsAppService()
 session_manager = SessionManager()
@@ -10,12 +13,17 @@ message_handler = MessageHandler(whatsapp_service, session_manager)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await session_manager.connect()
+    logger.info("[LIFESPAN] Application startup begins")
 
-    # This callback stays in the handler
+    await session_manager.connect()
+    logger.info("[LIFESPAN] Connected to Redis")
+
     await session_manager.start_ttl_watcher(
         on_expire_callback=message_handler.on_session_expired,
         interval_seconds=60
     )
+    logger.info("[LIFESPAN] TTL watcher started")
 
     yield
+
+    logger.info("[LIFESPAN] Application shutdown")

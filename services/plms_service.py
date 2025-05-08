a4a573587202e681749831ffa057c01100241c1d
Joseph Heykel Prabawa <josephheykel@gmail.com>
2025-05-08 11:48:08 +0700

    development/fixed | adding validation member and MEMBER menu

--- services/plms_service.py
+++ services/plms_service.py
@@ -15,7 +15,6 @@ class PLMSService:
     def login(self):
         text = PLMSUser.USERNAME.value + PLMSUser.PASSWORD.value + PLMSSecretKey.SECRET_KEY.value
         checksum = hashlib.sha256(text.encode()).hexdigest()
-        logger.info(f"CHECKSUM FOR LOGIN {checksum}")
     
         payload = {
             "username": PLMSUser.USERNAME.value,
@@ -60,9 +59,6 @@ class PLMSService:
             "checksum": checksum
         }
         
-        logger.info(payload)
-        
-
         try:
             response = requests.post(f"{self.endpoint}/validatemember", json=payload)
             data = response.json()

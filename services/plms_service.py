c599961b48eace56a4d68f341ecd2b47ea904d49
Joseph Heykel Prabawa <josephheykel@gmail.com>
2025-05-16 17:14:30 +0700

    development/testing/bugs fix |FLOW ACTIVATION MEMBER message handler - adding nfm_reply, service, controller - manage handle_nfm_reply contol

--- services/plms_service.py
+++ services/plms_service.py
@@ -43,8 +43,6 @@ class PLMSService:
         if not self.token:
             self.login()
         
-        phone_number = str(phone_number)
-        
         if phone_number.startswith("62"):
             phone_number = "0" + phone_number[2:]
             
@@ -79,17 +77,17 @@ class PLMSService:
             
         data = register_data
 
-        phone_number = str(data.get("phone_number", ""))
+        phone_number = data.get("phone_number", "")
         if phone_number.startswith("62"):
             phone_number = "0" + phone_number[2:]
         
-        name = str(data.get("name"))
-        birth_date = str(data.get("birth_date"))
-        email = str(data.get("email"))
-        card_number = str(data.get("card_number"))
-        gender = str(data.get("gender"))
-        marital = str(data.get("marital"))
-        address = str(data.get("address"))
+        name = data.get("name", "")
+        birth_date = data.get("birth_date", "")
+        email = data.get("email", "")
+        card_number = data.get("card_number", "")
+        gender = data.get("gender", "")
+        marital = data.get("marital", "")
+        address = data.get("address", "")
         
         try:
             birth_date = datetime.strptime(birth_date, "%Y-%m-%d").strftime("%d%m%Y")

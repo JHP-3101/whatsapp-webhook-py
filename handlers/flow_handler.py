2a85b6b091fa56877c6cb859cb2b9ee34ad56498
jspheykel <78401218+jspheykel@users.noreply.github.com>
2025-06-18 13:49:42 +0700

    dev/Developing Reset PIN Flow Menu, Feature added PIN Check and PIN Reset

--- handlers/flow_handler.py
+++ handlers/flow_handler.py
@@ -48,7 +48,7 @@ class FlowHandler:
                     
                 return await self.validate_birth_date(version, data, phone_number)
             
-            elif action == "complete" and screen == "RESET_PIN":
+            elif screen == "RESET_PIN":
                 phone_raw = data.get("phone_number")
                 logger.info(f"Phone Raw Data From Flow Reset PIN : {phone_raw}")
                 phone_number = phone_raw.get("value") if isinstance(phone_raw, dict) else phone_raw
